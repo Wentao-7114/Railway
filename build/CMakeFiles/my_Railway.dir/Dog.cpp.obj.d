e6b3cc326592a4621179598e12d79ea3e54beca1
wentaoz4 <wentaoz4@illinois.edu>
2021-12-28 23:07:36 -0500

    adding again

---
+++ build/CMakeFiles/my_Railway.dir/Dog.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/my_Railway.dir/Dog.cpp.obj: D:/Railway/Dog.cpp \
+ D:/Railway/Dog.h
---
+++ build/CMakeFiles/my_Railway.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_Railway.dir/Dog.cpp.obj"
+  "CMakeFiles/my_Railway.dir/Dog.cpp.obj.d"
+  "CMakeFiles/my_Railway.dir/main.cpp.obj"
+  "CMakeFiles/my_Railway.dir/main.cpp.obj.d"
+  "libmy_Railway.dll.a"
+  "my_Railway.exe"
+  "my_Railway.exe.manifest"
+  "my_Railway.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_Railway.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
